c1c85dc76a92fd77086e097bf3493defe0b977bc
lachlandk <lachlandufort-kennett@outlook.com>
2020-02-26 23:32:18 +0000

    Added minifying procedure to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 const gulp = require("gulp");
 const rename = require("gulp-rename");
-const uglify = require("gulp-uglify");
-const concat = require("gulp-concat");
+const uglify = require("uglify-es");
 const strip = require("gulp-strip-comments");
 const inject = require("gulp-inject");
 const del = require("del");
+const composer = require("gulp-uglify/composer");
+const minify = composer(uglify, console);
 
 async function clean(){
 	const paths = await del(["dist/node/**", "dist/web/**"])
@@ -37,9 +38,9 @@ function webify(){
 		.pipe(strip())
 		.pipe(rename("quasar-web.js"))
 		.pipe(gulp.dest("dist/web/"))
-		// .pipe(uglify())
-		// .pipe(rename({extname: ".min.js"}))
-		// .pipe(gulp.dest("dist/web/"));
+		.pipe(minify())
+		.pipe(rename({extname: ".min.js"}))
+		.pipe(gulp.dest("dist/web/"));
 }
 
 exports.build = gulp.series(clean, nodeify, webify);
