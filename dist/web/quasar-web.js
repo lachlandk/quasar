b78c5f97f18065e340ea90e571034b6d509007ff
lachlandk <lachlandufort-kennett@outlook.com>
2020-03-04 15:51:38 +0000

    Changed expression data structure to object binary tree

--- dist/web/quasar-web.js
+++ dist/web/quasar-web.js
@@ -1,6 +1,30 @@
 (function (root, constructor){
 	root.Quasar = new constructor();
 }(self, function (){
+	this.polynomialAdd = function(){
+
+	};
+
+	this.polynomialSubtract = function(){
+
+	};
+
+	this.polynomialMultiply = function(){
+
+	};
+
+	this.polynomialDivide = function(){
+
+	};
+
+	this.polynomialDerivative = function(){
+
+	};
+
+	this.polynomialIntegral = function(){
+
+	};
+
 	this.gcd = function(num1, num2){
 		let a = Math.max(Math.abs(num1), Math.abs(num2)),
 			b = Math.min(Math.abs(num1), Math.abs(num2));
@@ -38,70 +62,61 @@
 		return Math.abs(num1 * num2) / this.gcd(num1, num2);
 	};
 
-	this.Node = function(type, value, properties) {
+	this.Node = function(type, value){
 		this.type = type;
 		this.value = value;
-		this.properties = {};
-		for (const property in properties) {
-			if (properties.hasOwnProperty(property)) {
-				this.properties[property] = properties[property];
-			}
-		}
+		this.left = null;
+		this.right = null;
 	};
 
-	this.Expression = function(){
-		this.tree = [];
-
-		this.append = function(node){
-			this.tree.unshift(node);
-		}
+	this.Expression = function(root=null){
+		this.root = root;
 	};
 
 	this.parse = function(input){
 		const lexemes = input.match(/\d+|\S/ig);
-		let tokens = [];
+		const tokens = [];
 		lexemes.forEach(function(lexeme, i){
 			if (lexeme.match(/\d+/)){
-				tokens.push(new this.Node("constant", parseInt(lexeme)));
+				tokens.push({type: "constant", value: parseInt(lexeme)});
 			} else
 			if (lexeme.match(/[a-z]/i)){
-				tokens.push(new this.Node("variable", lexeme));
+				tokens.push({type: "variable", value: lexeme});
 			} else
 			if (lexeme.match(/[+\-*/^]/)){
 				if (lexeme.match(/\^/)){
-					tokens.push(new this.Node("operator", "^", {associativity: "right", precedence: 1}));
+					tokens.push({type: "operator", value: "^", associativity: "right", precedence: 1});
 				} else
 				if (lexeme.match(/[\/*]/)){
-					tokens.push(new this.Node("operator", lexeme, {associativity: "left", precedence: 2}));
+					tokens.push({type: "operator", value: lexeme, associativity: "left", precedence: 2});
 				} else
 				if (lexeme.match(/[+-]/)){
-					tokens.push(new this.Node("operator", lexeme, {associativity: "left", precedence: 3}));
+					tokens.push({type: "operator", value: lexeme, associativity: "left",   precedence: 3});
 				}
 			} else
-			if (lexeme.match(/\(/)){
-				tokens.push(new this.Node("open-parenthesis", "("));
+			if (lexeme.match(/[(\[]/)){
+				tokens.push({type: "open-parenthesis", value: lexeme});
 			} else
-			if (lexeme.match(/\)/)){
-				tokens.push(new this.Node("close-parenthesis", ")"));
+			if (lexeme.match(/[)\]]/)){
+				tokens.push({type: "close-parenthesis", value: lexeme});
+			} else
+			if (lexeme.match(/,/)){
+				tokens.push({type: "delimiter", value: lexeme});
 			} else {
-				throw {name: "LexingError", msg: "Unknown Token" + lexeme};
+				throw {name: "LexingError", msg: "Unknown Token: " + lexeme};
 			}
-		}, this);
+		});
 
 		for (let i=0; i<tokens.length; i++){
-			if (tokens[i].type === "function" && tokens[i+1].type !== "open-parenthesis" && (tokens[i+1].type === "variable" || tokens[i+1].type === "constant")){
-				tokens.splice(i+1, 0, new this.Node("open-parenthesis", "("));
-				tokens.splice(i+3, 0, new this.Node("close-parenthesis", ")"));
-			}
-			if (i !== 0 && (tokens[i].type === "variable" || tokens[i].type === "constant" || tokens[i].type === "open-parenthesis" || tokens[i].type === "function")){
+			if (i !== 0 && (tokens[i].type === "variable" || tokens[i].type === "constant" || tokens[i].type === "open-parenthesis")){
 				if (tokens[i-1].type === "variable" || tokens[i-1].type === "constant" || tokens[i-1].type === "close-parenthesis"){
-					tokens.splice(i, 0, new this.Node("operator", "*", {associativity: "left", precedence: 2}));
+					tokens.splice(i, 0, {type: "operator", value: "*", associativity: "left", precedence: 2})
 				}
 			} else
 			if (tokens[i].type === "operator"){
-				if (tokens[i].value.match(/[+-]/) && (i === 0 || tokens[i-1].type === "operator" || tokens[i-1].type === "open-parenthesis" || tokens[i-1].type === "delimiter")){
+				if (tokens[i].value.match(/[+-]/) && (i === 0 || tokens[i-1].type === "operator" || tokens[i-1].type === "open-parenthesis")){
 					if (tokens[i].value === "-"){
-						tokens[i] = new this.Node("operator", "~", {precedence: 0});
+						tokens[i] = {type: "operator", value: "~", precedence: 0};
 					} else {
 						tokens.splice(i, 1);
 					}
@@ -109,48 +124,62 @@
 			}
 		}
 
-		let output = new this.Expression();
-		let stack = [];
+		const output = [];
+		const stack = [];
 		while (tokens.length !== 0){
-			let token = tokens.shift();
+			const token = tokens.shift();
 			if (token.type === "constant" || token.type === "variable"){
-				output.append(token)
+				output.push(token);
 			} else
-			if (token.type === "function" || token.type === "open-parenthesis"){
-				stack.unshift(token)
+			if (token.type === "open-parenthesis"){
+				stack.unshift(token);
 			} else
-			if (token.type === "delimiter" || token.type === "close-parenthesis"){
+			if (token.type === "close-parenthesis"){
 				while (stack[0].type !== "open-parenthesis"){
-					output.append(stack.shift())
+					output.push(stack.shift());
 				}
-				if (token.type === "close-parenthesis"){
-					stack.shift();
-					if (stack.length !== 0 && stack[0].type === "function"){
-						output.append(stack.shift())
-					}
-				}
+				stack.shift();
 			} else
 			if (token.type === "operator"){
-				if (token.properties.associativity === "left"){
-					while (stack.length !== 0 && stack[0].type === "operator" && stack[0].properties.precedence <= token.properties.precedence){
-						output.append(stack.shift())
+				if (token.associativity === "left"){
+					while (stack.length !== 0 && stack[0].type === "operator" && stack[0].precedence <= token.precedence){
+						output.push(stack.shift());
 					}
-					stack.unshift(token)
+					stack.unshift(token);
 				} else
-				if (token.properties.associativity === "right"){
-					while (stack.length !== 0 && stack[0].properties.precedence < token.properties.precedence){
-						output.append(stack.shift())
+				if (token.associativity === "right"){
+					while (stack.length !== 0 && stack[0].precedence < token.precedence){
+						output.push(stack.shift());
 					}
-					stack.unshift(token)
+					stack.unshift(token);
 				} else {
-					stack.unshift(token)
+					stack.unshift(token);
 				}
 			}
 		}
 		while (stack.length !== 0){
-			output.append(stack.shift())
+			output.push(stack.shift());
 		}
-		return output
+
+		output.forEach(function(token, i){
+			if (token.type === "variable" || token.type === "constant"){
+				stack.unshift(new this.Node(token.type, token.value));
+			} else
+			if (token.type === "operator"){
+				if (token.precedence === 0){
+					let node = new this.Node(token.type, token.value);
+					node.left = stack.shift();
+					stack.unshift(node);
+				} else {
+					let node = new this.Node(token.type, token.value);
+					node.right = stack.shift();
+					stack.length > 0 ? node.left = stack.shift() : null;
+					stack.unshift(node);
+				}
+			}
+		}, this);
+
+		return new this.Expression(stack[0])
 	};
 
 }));
