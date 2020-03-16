61a75771a3498504936354e9c6c235c125ffa7a7
lachlandk <lachlandufort-kennett@outlook.com>
2020-03-16 19:33:23 +0000

    Added random_module.js

--- dist/node/quasar.js
+++ dist/node/quasar.js
@@ -1,49 +1,4 @@
 function Quasar(){
-	this.add = function(a, b){
-		const result = new this.Expression(new this.Node("operator", "+"));
-		result.root.left = a.root;
-		result.root.right = b.root;
-		return result
-	};
-
-
-	this.gcd = function(num1, num2){
-		let a = Math.max(Math.abs(num1), Math.abs(num2)),
-			b = Math.min(Math.abs(num1), Math.abs(num2));
-		while (b !== 0){
-			let r = a % b;
-			a = b;
-			b = r;
-		}
-		return a;
-	};
-
-	this.bezout = function(num1, num2){
-		let a = Math.max(Math.abs(num1), Math.abs(num2)),
-			b = Math.min(Math.abs(num1), Math.abs(num2));
-		let a1 = 1,
-			b1 = 0,
-			a2 = 0,
-			b2 = 1;
-		while (b !== 0){
-			let q = Math.floor(a / b),
-				r = a - q*b,
-				r1 = a1 - q*b1,
-				r2 = a2 - q*b2;
-			a = b;
-			a1 = b1;
-			a2 = b2;
-			b = r;
-			b1 = r1;
-			b2 = r2;
-		}
-		return [a2, a1]
-	};
-
-	this.lcm = function(num1, num2) {
-		return Math.abs(num1 * num2) / this.gcd(num1, num2);
-	};
-
 	this.Node = function(type, value, properties){
 		this.type = type;
 		this.value = value;
@@ -268,6 +223,56 @@ function Quasar(){
 
 
 		return expression;
+	};
+
+	this.add = function(a, b){
+		const result = new this.Expression(new this.Node("operator", "+"));
+		result.root.left = a.root;
+		result.root.right = b.root;
+		return result
+	};
+
+
+	this.randomNumber = function(start=0, end=10){
+		return Math.round(Math.random()*(end-start) + start);
+	}
+
+
+	this.gcd = function(num1, num2){
+		let a = Math.max(Math.abs(num1), Math.abs(num2)),
+			b = Math.min(Math.abs(num1), Math.abs(num2));
+		while (b !== 0){
+			let r = a % b;
+			a = b;
+			b = r;
+		}
+		return a;
+	};
+
+	this.bezout = function(num1, num2){
+		let a = Math.max(Math.abs(num1), Math.abs(num2)),
+			b = Math.min(Math.abs(num1), Math.abs(num2));
+		let a1 = 1,
+			b1 = 0,
+			a2 = 0,
+			b2 = 1;
+		while (b !== 0){
+			let q = Math.floor(a / b),
+				r = a - q*b,
+				r1 = a1 - q*b1,
+				r2 = a2 - q*b2;
+			a = b;
+			a1 = b1;
+			a2 = b2;
+			b = r;
+			b1 = r1;
+			b2 = r2;
+		}
+		return [a2, a1]
+	};
+
+	this.lcm = function(num1, num2) {
+		return Math.abs(num1 * num2) / this.gcd(num1, num2);
 	};
 
 }
---
+++ src/random_module.js
@@ -0,0 +1,15 @@
+	this.randomNumber = function(start=0, end=10){
+		return Math.round(Math.random()*(end-start) + start);
+	}
+
+	// this.randomPolynomialExp = function(){
+	//
+	// };
+	//
+	// this.randomRationalExp = function(){
+	//
+	// };
+	//
+	// this.randomTranscendExp = function(){
+	//
+	// };
