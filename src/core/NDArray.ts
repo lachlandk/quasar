67900072e23c6aac46007585f0f2381523a1240d
LachlanD-K <lachlandufort-kennett@outlook.com>
2022-02-04 00:03:49 +0000

    Added validation for indices in get and set methods for NDArray

--- src/core/NDArray.ts
+++ src/core/NDArray.ts
@@ -19,8 +19,8 @@ export class NDArray {
         this.dtype = dtype;
         this.itemSize = 8;  // float64
 
-        if (!shape.every((size) => Number.isInteger(size))) throw `Error: Size of array dimensions must be integer`;  // TODO: proper errors
-        if (Array.isArray(strides) && !strides.every((stride) => Number.isInteger(stride / this.itemSize))) throw `Error: Size of array strides must be integer multiple of itemSize`;
+        if (!shape.every(size => Number.isInteger(size))) throw `Error: Size of array dimensions must be integer`;  // TODO: proper errors
+        if (Array.isArray(strides) && !strides.every(stride => Number.isInteger(stride / this.itemSize))) throw `Error: Size of array strides must be integer multiple of itemSize`;
         if (!Number.isInteger(offset / this.itemSize)) throw `Error: Array offset must be multiple of itemSize`;
 
         // calculate data size and dimension
@@ -48,21 +48,26 @@ export class NDArray {
     }
 
     get(...indices: number[]): number {
-        // TODO: allows illegal indices
+        indices.fill(0, indices.length, this.dimension);
+        if (!indices.every((index, i) => index < this.shape[i])) throw `Error: Index out of bounds`;
         return this.data.getFloat64(this.strides.reduce((acc, curr, i) => acc + curr * indices[i], 0));
     }
 
     set(value: number, ...indices: number[]) {
-        // TODO: allows illegal indices
+        indices.fill(0, indices.length, this.dimension);
+        if (!indices.every((index, i) => index < this.shape[i])) throw `Error: Index out of bounds`;
         this.data.setFloat64(this.strides.reduce((acc, curr, i) => acc + curr * indices[i], 0), value);
     }
 
     slice(...axes: [number, number, number][]): NDArray {  // [start, end, step]
 
         // check that passed indices are valid
-        axes.forEach(ax => {
+        axes.forEach((ax, i) => {
+            if (!(Number.isInteger(ax[0]) && Number.isInteger(ax[1]) && Number.isInteger(ax[2]))) throw `Error: Indices must be integers`;
+            if (!(ax[0] < this.shape[i] && ax[1] <= this.shape[i])) throw `Error: Index out of bounds`;
             if (ax[1] <= ax[0]) throw `Error: stop index cannot be higher than or equal to start index`;
             if (ax[2] === 0) throw `Error: step cannot be 0`;
+            if (ax[2] < 0) throw `Error: Negative step not currently supported`;
         });
 
         // pad out axes list
