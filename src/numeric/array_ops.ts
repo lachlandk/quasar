16ef8d9bd6aa545bdd822839f9b87b3d8f07c594
LachlanD-K <lachlandufort-kennett@outlook.com>
2022-02-26 11:58:32 +0000

    Created `array_ops.ts` with some basic array operations

---
+++ src/numeric/array_ops.ts
@@ -0,0 +1,34 @@
+import { UFunc, NDArray } from "../core/index.js";
+
+export function transpose(x1: NDArray): NDArray {
+    // assumes dtype float64
+    return new NDArray(x1.shape.reverse(), "float64", x1.data.buffer, 0, x1.strides.reverse());
+}
+
+export const add: (x1: NDArray, x2: NDArray) => NDArray = UFunc(function add(x1: number, x2: number) {
+    return x1 + x2;
+});
+
+export const subtract: (x1: NDArray, x2: NDArray) => NDArray = UFunc(function subtract(x1: number, x2: number) {
+    return x1 - x2;
+});
+
+export const multiply: (x1: NDArray, x2: NDArray) => NDArray = UFunc(function multiply(x1: number, x2: number) {
+    return x1 * x2;
+});
+
+export const divide: (x1: NDArray, x2: NDArray) => NDArray = UFunc(function divide(x1: number, x2: number) {
+    return x1 / x2;
+});
+
+export const power: (x1: NDArray, x2: NDArray) => NDArray = UFunc(function power(x1: number, x2: number) {
+    return x1 ** x2;
+});
+
+export const mod: (x1: NDArray, x2: NDArray) => NDArray = UFunc(function mod(x1: number, x2: number) {
+    return x1 % x2;
+});
+
+export const reciprocal: (x1: NDArray) => NDArray = UFunc(function reciprocal(x1: number) {
+    return 1 / x1;
+});
