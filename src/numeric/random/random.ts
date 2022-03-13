65566b184096659cd5675ef701bdb30c38a6c1d8
LachlanD-K <lachlandufort-kennett@outlook.com>
2022-03-13 18:37:15 +0000

    Added `randint` function

--- src/numeric/random/random.ts
+++ src/numeric/random/random.ts
@@ -26,3 +26,18 @@ export function uniform(min: number = 0, max: number = 1, size?: number[]) {
         return array;
     }
 }
+
+// export function randint(max: number, size?: number[]): number | NDArray
+// export function randint(min: number, max: number, size?: number[]): number | NDArray
+// export function randint(minOrMax: number, maxOrSize?: number | number[], size?: number[]) {
+export function randint(min: number = 0, max: number = 1, size?: number[]) {
+    if (size === undefined) {
+        return Math.floor(Math.random() * (max - min) + min);
+    } else {
+        const array = new NDArray(size);
+        array.forEach(function (element, indices) {
+            array.set(Math.floor(Math.random() * (max - min) + min), ...indices);
+        });
+        return array;
+    }
+}
