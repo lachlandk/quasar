2529c7b273dcc014ccf164c3dcd8e4e4c52da668
LachlanD-K <lachlandufort-kennett@outlook.com>
2022-03-13 18:31:03 +0000

    Added `uniform` function

--- src/numeric/random/random.ts
+++ src/numeric/random/random.ts
@@ -11,3 +11,18 @@ export function random(size?: number[]) {
         return array;
     }
 }
+
+export function uniform(min: number = 0, max: number = 1, size?: number[]) {
+    // p(x) = { 1 / (max - min) for x in [min, max), 0 otherwise
+    min = min === undefined ? 0 : min;
+    max = max === undefined ? 1 : max;
+    if (size === undefined) {
+        return Math.random() * (max - min) + min;
+    } else {
+        const array = new NDArray(size);
+        array.forEach(function (element, indices) {
+            array.set(Math.random() * (max - min) + min, ...indices);
+        });
+        return array;
+    }
+}
